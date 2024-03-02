ed8025e8825014ecceaca8500dbd5819ff93a7f2
MTGrimm <98679259+MTGrimm@users.noreply.github.com>
2024-03-02 14:48:46 -0700

    first commit

---
+++ ascii.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "image"
+    "image/png"
+    "os"
+)
+
+func main() {
+    // Checking a command-line argument was passed
+    if len(os.Args) < 2 {
+        fmt.Println("Please input file name")
+        os.Exit(1)
+    }
+
+    // Reading the file name
+
+}   
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module ascii.go
+
+go 1.21.4
